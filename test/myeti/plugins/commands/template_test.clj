97d22d908fd309a7e9f0297c194387aa88762f41
gkspranger <greg@spranger.us>
2020-05-16 19:09:10 -0400

    yay local vars

--- test/myeti/plugins/commands/template_test.clj
+++ test/myeti/plugins/commands/template_test.clj
@@ -3,10 +3,10 @@
             [myeti.plugins.commands.template :as template]))
 
 (deftest template-cmd-test
-  (is (clojure.string/includes?
-       (template/template-cmd {:match [0 "Greg"]})
-       "Hello Greg"))
-  (is (clojure.string/includes?
-       (template/template-cmd {:match [0 "Greg"]})
-       "are doing well today")))
-
+  (let [match {:match [0 "Greg"]}]
+    (is (clojure.string/includes?
+         (template/template-cmd match)
+         "Hello Greg"))
+    (is (clojure.string/includes?
+         (template/template-cmd match)
+         "are doing well today"))))
